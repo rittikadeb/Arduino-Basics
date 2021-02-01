c654de52b1f0227882a1b76b1455da840174395f
RITTIKA DEB <76259897+rittikadeb@users.noreply.github.com>
2021-02-02 00:46:00 +0530

    Update Blinking of LED.ino

--- Blinking of LED.ino
+++ Blinking of LED.ino
@@ -4,7 +4,7 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turns the LED on )
+  digitalWrite(LED_BUILTIN, HIGH);   // turns the LED on 
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turns the LED off 
   delay(1000);                       // wait for a second
